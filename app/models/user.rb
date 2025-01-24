d04d391f29691f23c3cf4e892a7daaeb52093979
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-24 21:37:46 +0900

    ストロングパラメータ追加、アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # アソシエーション
+  has_many :job_posts, foreign_key: "owner_id", dependent: :destroy
+
 end
