98a4714fd6501d4cc63c9177d9365a26c97e71b5
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-25 16:49:24 +0900

    setup-rspec-factorybot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_24_025101) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_25_070058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_24_025101) do
     t.string "qualification", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "experience"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
